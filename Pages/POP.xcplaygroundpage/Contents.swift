99d165237e1b314a6657e75312be66459c823815
Sergio Mendes <sergiomendes@MacBook-Air-de-Sergio.local>
2023-12-29 12:38:22 -0300

    applied extension

--- Pages/POP.xcplaygroundpage/Contents.swift
+++ Pages/POP.xcplaygroundpage/Contents.swift
@@ -41,3 +41,31 @@ struct Parrot: Bird, Flyable {
 }
 let parrot = Parrot(name: "Papagaio", amplitude: 12.0, frequency: 5.0)
 print("O \(parrot.name) voa na velocidade máixima de \(parrot.maximumSpeed) km/h")
+
+struct Penguin {
+    let name: String
+    let canFly: Bool = false
+}
+let penguin = Penguin(name: "Pinguim de Madagascar")
+print("\(penguin.name) é um pássaro que voa? \(penguin.canFly ? "Sim!" : "Não, mas é detetive e faz missões!")")
+
+extension Bird {
+    var canFly: Bool {self is Flyable}
+}
+
+struct Eagle: Bird, Flyable{
+    let name: String
+    let amplitude: Double
+    let frequency: Double
+
+    var maximumSpeed: Double {
+        3 * amplitude * frequency
+    }
+}
+struct Chick: Bird{
+    let name: String
+}
+let eagle = Eagle(name: "Águia", amplitude: 13.0, frequency: 4.0)
+print("\(eagle.name) voa? \(eagle.canFly ? "Sim!": "Não!") E mergulha para caçar na velocidade de \(eagle.maximumSpeed) KM/h")
+let chick = Chick(name: "Pintinho piu")
+print("\(chick.name) voa? \(chick.canFly ? "Sim!": "Não!") Só pia... piu piu piu")
