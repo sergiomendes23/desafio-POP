179751db7fc36de0f0f29c7ba397aea59099bba8
Sergio Mendes <sergiomendes@MacBook-Air-de-Sergio.local>
2023-12-29 12:19:56 -0300

    new protocol

--- Pages/POP.xcplaygroundpage/Contents.swift
+++ Pages/POP.xcplaygroundpage/Contents.swift
@@ -20,3 +20,24 @@ struct SergioAccount: BankAccount, AccountBalance {
 }
 let saldo = SergioAccount(name: "Sérgio", debit: 1000, credit: 300)
 print("O \(saldo.name) ganhou um total de \(saldo.debit) reais e gastou \(saldo.credit) reais. Sobrou pra ele \(saldo.balance) reais")
+
+protocol Bird {
+    var name: String { get }
+    var canFly: Bool { get }
+}
+protocol Flyable {
+    var maximumSpeed: Double { get }
+}
+
+struct Parrot: Bird, Flyable {
+    let name: String
+    let amplitude: Double
+    let frequency: Double
+    let canFly: Bool = true
+
+    var maximumSpeed: Double {
+        3 * amplitude * frequency
+    }
+}
+let parrot = Parrot(name: "Papagaio", amplitude: 12.0, frequency: 5.0)
+print("O \(parrot.name) voa na velocidade máixima de \(parrot.maximumSpeed) km/h")
